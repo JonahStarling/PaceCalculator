93f6a1206c5149a5a520c753d8740b13b8ba166d
Jonah Starling <starling.jonah@gmail.com>
2020-03-03 21:58:14 -0600

    App Created

---
+++ Jonah's Pace Calculator/Jonah's Pace Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jonah's Pace Calculator
+//
+//  Created by Jonah Starling on 3/3/20.
+//  Copyright © 2020 Jonah Starling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Jonah's Pace Calculator/Jonah's Pace CalculatorTests/Jonah_s_Pace_CalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Jonah_s_Pace_CalculatorTests.swift
+//  Jonah's Pace CalculatorTests
+//
+//  Created by Jonah Starling on 3/3/20.
+//  Copyright © 2020 Jonah Starling. All rights reserved.
+//
+
+import XCTest
+@testable import Jonah_s_Pace_Calculator
+
+class Jonah_s_Pace_CalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
